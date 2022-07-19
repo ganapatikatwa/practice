76f085b74003cd9db6f04deee0d35334edf47efa
[ganapat] <ganapatisakatwa@gmail.com>
2022-07-19 07:20:20 +0530

    [ganapati]SINGLE DIGIT NUMBER IS WORD USING CASE STATEMENT

---
+++ case1.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+Number=$1
+case $Number in
+"0")
+echo "$Number is Zero";;
+"1")
+echo "$Number is One";;
+"2")
+echo "$Number is Two";;
+"3")
+echo "$Number is Three";;
+"4")
+echo "$Number is Four";;
+"5")
+echo "$Number is Five";;
+"6")
+echo "$Number is Six";;
+"7")
+echo "$Number is Seven";;
+"8")
+echo "$Number is Eight";;
+"9")
+echo "$Number is Nine";;
+esac
